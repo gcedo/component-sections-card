a05e81f172ae89e97760871bee194c390ef4dd17
Edoardo Colombo <edoardocolombo@economist.com>
2016-03-08 17:22:24 +0000

    chore: provision

--- src/index.js
+++ src/index.js
@@ -2,102 +2,113 @@ import Button from '@economist/component-link-button';
 import List from '@economist/component-list';
 import React from 'react';
 
-export default class SectionsCard extends React.Component {
-
-  static get propTypes() {
-    return {
-      data: React.PropTypes.object, // eslint-disable-line id-blacklist
-      titles: React.PropTypes.shape({
-        blogs: React.PropTypes.string,
-        sections: React.PropTypes.string,
-      }),
+function renderListContent(linkList) {
+  return linkList.map((link, i) => {
+    const commonProps = {
+      key: `${ i }`,
+      unstyled: true,
+      ...link,
     };
-  }
-
-  static get defaultProps() {
-    return {
-      titles: {
-        blogs: 'Blogs',
-      },
-    };
-  }
-
-  targetIfNeeded({ internal }) {
-    if (internal === false) {
-      return { target: '_blank' };
-    }
-    return {};
-  }
-  renderListContent(linkList) {
-    return linkList.map((link, i) => {
-      const commonProps = {
-        key: `${ i }`,
-        unstyled: true,
-        ...link,
-      };
-      if (link.internal === false) {
-        return (
-          <Button
-            key={`sections-card__link_${ i }`}
-            className="sections-card__link sections-card__link--external"
-            {...commonProps}
-            target="_blank"
-            unstyled
-          >
-            {link.title}
-          </Button>
-        );
-      }
+    if (link.internal === false) {
       return (
         <Button
           key={`sections-card__link_${ i }`}
-          className="sections-card__link"
+          className="sections-card__link sections-card__link--external"
           {...commonProps}
+          target="_blank"
           unstyled
         >
-        {link.title}
-      </Button>);
-    });
-  }
-  render() {
-    const sectionsCardData = this.props.data;
-    const { titles } = this.props;
-    const sections = [];
-    if (titles.sections) {
-      sections.push(<h4 className="sections-card__header">{titles.sections}</h4>);
+          {link.title}
+        </Button>
+      );
     }
-    sections.push(
-      <List>
-        {this.renderListContent(sectionsCardData.sections)}
-      </List>
-    );
-    const blogs = [];
-    if (titles.blogs) {
-      blogs.push(<h4 className="sections-card__header">{titles.blogs}</h4>);
-    }
-    blogs.push(
-      <List>
-        {this.renderListContent(sectionsCardData.blogs)}
-      </List>
-    );
     return (
-      <nav role="nav" className="sections-card">
-        <div className="sections-card__wrapper">
-          <div className="sections-card__menu">
-            <div className="sections-card__list sections-card__list-sections">
-              {sections}
-            </div>
-            <div className="sections-card__list sections-card__list-media">
-              <List>
-                {this.renderListContent(sectionsCardData.media)}
-              </List>
-            </div>
-            <div className="sections-card__list sections-card__list-blogs">
-              {blogs}
-            </div>
+      <Button
+        key={`sections-card__link_${ i }`}
+        className="sections-card__link"
+        {...commonProps}
+        unstyled
+      >
+      {link.title}
+    </Button>);
+  });
+}
+
+function SectionsCard({ data, titles }) { // eslint-disable-line id-blacklist
+  const sectionsCardData = data; // eslint-disable-line id-blacklist
+  const sections = [];
+  if (titles.sections) {
+    sections.push(<h4 className="sections-card__header">{titles.sections}</h4>);
+  }
+  sections.push(
+    <List>
+      {renderListContent(sectionsCardData.sections)}
+    </List>
+  );
+  const blogs = [];
+  if (titles.blogs) {
+    blogs.push(<h4 className="sections-card__header">{titles.blogs}</h4>);
+  }
+  blogs.push(
+    <List>
+      {renderListContent(sectionsCardData.blogs)}
+    </List>
+  );
+
+  return (
+    <nav role="nav" className="sections-card">
+      <div className="sections-card__wrapper">
+        <div className="sections-card__menu">
+          <div className="sections-card__list sections-card__list-sections">
+            {sections}
+          </div>
+          <div className="sections-card__list sections-card__list-media">
+            <List>
+              {renderListContent(sectionsCardData.media)}
+            </List>
+          </div>
+          <div className="sections-card__list sections-card__list-blogs">
+            {blogs}
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
+
+if (process.env.NODE_ENV !== 'production') {
+  SectionsCard.propTypes = {
+    data: React.PropTypes.shape({ // eslint-disable-line id-blacklist
+      blogs: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          href: React.PropTypes.string,
+          title: React.PropTypes.string,
+        })
+      ),
+      media: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          href: React.PropTypes.string,
+          title: React.PropTypes.string,
+        })
+      ),
+      sections: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          href: React.PropTypes.string,
+          title: React.PropTypes.string,
+        })
+      ),
+    }),
+    titles: React.PropTypes.shape({
+      blogs: React.PropTypes.string,
+      sections: React.PropTypes.string,
+    }),
+  };
+}
+
+SectionsCard.defaultProps = {
+  titles: {
+    blogs: 'Blogs',
+  },
+};
+
+export default SectionsCard;
--- test/index.js
+++ test/index.js
@@ -5,12 +5,8 @@ import chai from 'chai';
 import context from '../src/context';
 chai.should();
 
-/* eslint-disable newline-after-var */
 describe('A SectionsCard', () => {
   describe('it\'s a React component', () => {
-    it('is compatible with React.Component', () => {
-      SectionsCard.should.be.a('function').and.respondTo('render');
-    });
     it('it\'s renders a React element', () => {
       React.isValidElement(<SectionsCard data={context} />).should.equal(true);
     });
